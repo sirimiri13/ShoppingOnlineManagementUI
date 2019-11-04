5342a784cd2634227a38e3dd0ab10a304aff2f19
HUONG <lamquynhhuong1304@gmail.com>
2019-11-04 15:03:02 +0700

    done

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+  router.get('/', function(req, res, next) {
+    res.render('index', { title: 'index' });
+  });
+  router.get('/logout', function(req, res, next) {
+    res.render('index', { title: 'index' });
+  });
+  router.get('/home', function(req, res, next) {
+    res.render('index', { title: 'index' });
+  });
+  router.get('/login', function(req, res, next) {
+    res.render('login', { title: 'login' });
+  });
+
+  router.get('/category', function(req, res, next) {
+    res.render('category', { title: 'category' });
+  });
+
+  router.get('/user', function(req, res, next) {
+    res.render('user', { title: 'user' });
+  });
+
+  router.get('/order', function(req, res, next) {
+    res.render('order', { title: 'oder' });
+  });
+  router.get('/product', function(req, res, next) {
+    res.render('product', { title: 'product' });
+  });
+  router.get('/store', function(req, res, next) {
+    res.render('store', { title: 'store' });
+  });
+
+module.exports = router;
